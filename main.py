9619d37d6a5d30f0afbf5e27da9d1fc20d66d68d
AAT8B <mstfyslamalsray@gmail.com>
2026-08-12 13:56:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,29 @@
-import time, redis, os, json, re, requests, asyncio, sys
-from pyrogram import *
+import time, os, json, re, requests, asyncio, sys
+from pyrogram import Client, idle
 
-# 1. تعريف المتغيرات الأساسية فوراً
-to_config = ""
-token = os.getenv("8063254835:AAHxabPeLMA4t4_R33MI8nP__8hfZR8g7uE")
-owner_id = int(os.getenv("8526612004"))
-
-if not token:
-    raise RuntimeError("BOT_TOKEN is missing")
-
-if not owner_id:
-    raise RuntimeError("OWNER_ID is missing")
-
-Dev_Neptune = token.split(":")[0]
-
-
-# 2. محاولة الاتصال بـ Redis
-try:
-    r = redis.Redis('localhost', decode_responses=True)
-    r.ping()
-except:
-    print("[-] Error: Redis is not running. Please run: sudo systemctl start redis-server")
-    sys.exit()
+# 1. تعريف المتغيرات الأساسية من Environment Variables
+token = os.environ.get("8063254835:AAHxabPeLMA4t4_R33MI8nP__8hfZR8g7uE")
+owner_id = int(os.environ.get("8526612004"))
+Dev_Neptune = token.split(':')[0]
 
 print('''
 Loading…
 █▒▒▒▒▒▒▒▒▒''')
 
-# 3. جلب البيانات
+# 2. جلب البيانات وحفظها
 try:
     from information import *
     Dev_Neptune = token.split(':')[0]
-    r.set(f'{Dev_Neptune}botowner', owner_id)
 except:
-    token = input ('[+] Enter the bot token : ')
-    Dev_Neptune = token.split(':')[0]
-    owner_id = int(input('[+] Enter SUDO ID : '))
-    r.set(f'{Dev_Neptune}botowner', owner_id)
-    with open ('information.py','w+') as www:
+    with open('information.py','w+') as www:
         www.write(f'token = "{token}"\nowner_id = {owner_id}')
 
 print('''
 10% 
 ███▒▒▒▒▒▒▒ ''')
 
-# 4. تجهيز ملف الإعدادات
-to_config = "import redis\nr = redis.Redis('localhost',decode_responses=True)\n"
+# 3. تجهيز ملف الإعدادات config.py
+to_config = "import redis\n"
 to_config += f"token = '{token}'\n"
 to_config += f"Dev_Neptune = token.split(':')[0]\n"
 to_config += f"sudo_id = {owner_id}\n"
@@ -73,11 +51,8 @@ print('''
 50% 
 ███████▒▒▒ ''')
 
-# 5. تشغيل التطبيق
+# 4. تشغيل التطبيق
 app = Client(f'{Dev_Neptune}Neptune', 28850159, '09a3e7d212b434aec973ad5ea10d8ec6', bot_token=token, plugins={"root": "Plugins"})
-
-if not r.get(f'{Dev_Neptune}:botkey'): r.set(f'{Dev_Neptune}:botkey', '⇜')
-if not r.get(f'{Dev_Neptune}botname'): r.set(f'{Dev_Neptune}botname', 'Jack')
 
 app.start()
 print("• 𝖲𝖮𝖴𝖱𝖢𝖤 𝖩𝖠𝖢𝖪 𝖨𝖲 𝖴𝖯 𝖠𝖭𝖣 𝖱𝖴𝖭𝖭I𝖭𝖦 ...")
